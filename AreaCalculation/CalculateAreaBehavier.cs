6ca50623d9df360f7b328b84f6f59141452d80df
Edgar Sarkisyan <hxxperr999@gmail.com>
2023-07-27 00:48:38 +0300

    Добавьте файлы проекта.

---
+++ AreaCalculation/CalculateAreaBehavier.cs
@@ -0,0 +1,50 @@
+﻿namespace AreaCalculation
+{
+    public interface CalculateAreaBehavier
+    {
+        public double AreaCalc(params double[] parameterArray);
+    }
+    class CircleAreaCalc : CalculateAreaBehavier
+    {
+        /// <summary>
+        /// Площадь круга по радиусу
+        /// </summary>
+        /// <param name="parameterArray">Радиус</param>
+        /// <returns></returns>
+        public double AreaCalc(params double[] parameterArray)
+        {
+            if (parameterArray.Length > 1)
+            {
+                throw new Exception("It's can be only one radius");
+            }
+            return Math.PI * parameterArray[0];
+        }
+    }
+    public class TriangleAreaCalc : CalculateAreaBehavier
+    {
+        /// <summary>
+        /// Площадь треугольника по трем сторонам
+        /// </summary>
+        /// <param name="parameterArray">Стороны треугольника</param>
+        /// <returns></returns>
+        public double AreaCalc(params double[] parameterArray)
+        {
+            if (parameterArray.Length > 3)
+            {
+                throw new Exception("It's can be only three sides");
+            }
+            double perimetr = 0;
+            foreach (double num in parameterArray)
+            {
+                perimetr += num;
+            }
+            perimetr /= 2;
+
+            double firstSide = parameterArray[0];
+            double secondSide = parameterArray[1];
+            double thirdSide = parameterArray[2];
+
+            return Math.Sqrt(perimetr * (perimetr - firstSide) * (perimetr - secondSide) * (perimetr - thirdSide));
+        }
+    }
+}
---
+++ AreaCalculation/Shape.cs
@@ -0,0 +1,35 @@
+﻿namespace AreaCalculation
+{
+    public abstract class Shape
+    {
+        public CalculateAreaBehavier calculateAreaBehavier;
+
+        public Shape() { }
+
+        public void SetCalculateAreaBehavier(CalculateAreaBehavier cab)
+        {
+            calculateAreaBehavier = cab;
+        }
+
+        public double calcArea(params double[] parameterArray)
+        {
+            return calculateAreaBehavier.AreaCalc(parameterArray);
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle()
+        {
+            calculateAreaBehavier = new CircleAreaCalc();
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle()
+        {
+            calculateAreaBehavier = new TriangleAreaCalc();
+        }
+    }
+}
